67e717347df7ebe63012f59be0f6e82e2adcf0ee
yohei shimizu <yohei.shimizu.1190@gmail.com>
2023-09-20 14:48:00 +0900

    Initial Commit

---
+++ CycleSnap/CycleSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  CycleSnapApp.swift
+//  CycleSnap
+//
+//  Created by yohei shimizu on 2023/09/20.
+//
+
+import SwiftUI
+
+@main
+struct CycleSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
